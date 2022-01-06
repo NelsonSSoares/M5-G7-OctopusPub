b5032d543b2e548f182d1c561783a2b50d5fee91
Nelson <punkpunkada@gmail.com>
2022-01-06 18:01:29 -0300

    inicio pag comida inputs e preestilos

--- src/components/Comidas/Comidas.js
+++ src/components/Comidas/Comidas.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import Header from '../Header/Header'
-import Footer from '../Footer/Footer'
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import styles from './styles.module.css';
+
 
 function Comidas() {
 
+  const [cod, setCod] = useState('');
+  const [nome, setNome] = useState('');
+  const [descricao, setDescriocao] = useState('');
+  const [preco, setPreco] = useState('');
+  const [img, setImg] = useState('');
+
+
   return (
-    <div>
+    <section className={styles.formulario}>
         <Header />
-        <h1>Comidas</h1>
+        {/*CAROUSEL/MODAL BANNER*/}
+        <h1 className={styles.title}>Comidas</h1>
+        <form>
+          <label for='cod'>Cod da Comida:</label>
+          <input name="cod" className={styles.inputText} type="text" placeholder='001'/>
+          <label for='nome'>Nome:</label>
+          <input name="nome" className={styles.inputText} type="text" placeholder='X-Salada'/>
+          <label for='descricao'>Descrição:</label>
+          <input name="descricao" className={styles.inputText} type="text" placeholder='Alface, tomate'/>
+          <label for='preco'>Preço:</label>
+          <input name="preco" className={styles.inputText} type="number" placeholder='R$30,00'/>
+          <label for='img'>imagem:</label>
+          <input name="img" className={styles.inputText} type="text" placeholder='imagem'/>
+        </form>
+
+        {/* POSSIVEL TABELA COM TODAS AS COMIDAS*/}
         <Footer />
-    </div>
+    </section>
   );
 }
 
