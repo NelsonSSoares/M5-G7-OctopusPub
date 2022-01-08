819095a911878779997faca35c0724ffad736132
Bárbara Cruz <ba.luizacruz@gmail.com>
2022-01-08 17:33:20 -0300

    feat: modal de novo pedido.

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -6,9 +6,18 @@ function Modal(props) {
   const [loading, setLoading] = useState(true);
   const [pedidos, setPedidos] = useState([]);
   const [modal, setModal] = useState(false);
+  const [header, setHeader] = useState(null);
+  const [body, setBody] = useState(null);
+
 
   useEffect(() => {
     setModal(props.modal)
+
+    const {children} = props
+
+    setHeader(children.find(child => child.key === 'header'))
+    setBody(children.find(child => child.key === 'body'))
+
   }, [props.modal]);
 
   return (
@@ -16,14 +25,14 @@ function Modal(props) {
       <div className={modal ? styles.modal : styles.hidden} id="modal-one" aria-hidden="true">
         <div className={styles.modalDialog}>
           <div className={styles.modalHeader}>
-            <h2>Modal in CSS?</h2>
+          {header ? header.props.children : null}
             <a href="#" className={styles.btnClose} aria-hidden="true" onClick={props.closeModal}>×</a>
 
           </div>
           <div className={styles.modalBody}>
-            <p>One modal example here! :D</p>
+          {body ? body.props.children : null}
           </div>
-          <div className={styles.modalFooter}> <a href="#" className={styles.btn}>Nice!</a>
+          <div className={styles.modalFooter}> <a href="#" className={styles.btn} onClick={props.save}>Salvar</a>
 
           </div>
         </div>
--- src/components/Pedidos/Pedidos.js
+++ src/components/Pedidos/Pedidos.js
@@ -11,18 +11,53 @@ function Pedidos() {
   const [loading, setLoading] = useState(true);
   const [pedidos, setPedidos] = useState([]);
   const [modal, setModal] = useState(false);
+  const [numero, setNumero] = useState('');
+  const [comidas, setComidas] = useState([])
+  const [bebidas, setBebidas] = useState([])
+
+
+  function salvarPedido() {
+    console.log('salvou')
+    setModal(false)
+  }
+
+  async function carregarComandas() {
+    setLoading(true);
+    const response = await axios.get(
+      `http://localhost:3001/comandas/`
+    );
+
+    setPedidos(response.data);
+    setLoading(false);
+  }
+
+  async function carregarComidas() {
+    const response = await axios.get(
+      `http://localhost:3001/comidas/`
+    );
+
+    setComidas(response.data)
+  }
+
+  async function carregarBebidas() {
+    const response = await axios.get(
+      `http://localhost:3001/bebidas/`
+    );
+
+    setBebidas(response.data)
+  }
+
+  async function novoPedido() {
+    await carregarComidas()
+    await carregarBebidas()
+    setModal(true)
+  }
+
+  async function abrirPedido() {
+    console.log('abriu')
+  }
 
   useEffect(() => {
-    async function carregarComandas() {
-      setLoading(true);
-      const response = await axios.get(
-        `http://localhost:3001/comandas/`
-      );
-
-      setPedidos(response.data);
-      setLoading(false);
-    }
-
     carregarComandas();
   }, []);
 
@@ -41,7 +76,7 @@ function Pedidos() {
           (
             <>
               <tr key={p.id} className={styles.tr}>
-                <td> <a href="#" key={p.id} onClick={() => setModal(true)}>{p.numero}</a></td>
+                <td> <a href="#" key={p.id} onClick={abrirPedido}>{p.numero}</a></td>
                 <td>{p.total_pedido}</td>
               </tr>
             </>
@@ -50,8 +85,51 @@ function Pedidos() {
           }
         </tbody>
       </table>
+      <input type="button" value="Novo Pedido" onClick={novoPedido}/>
 
-      <Modal modal={modal} closeModal={() => setModal(false)} />
+      <Modal
+        modal={modal}
+        closeModal={() => setModal(false)}
+        save={salvarPedido}
+      >
+        <div key={'header'}><span>Pedido</span></div>
+        <div key={'body'}>
+          <label>Número da comanda:</label>
+          <input
+            value={numero}
+            type="text"
+            placeholder='001'
+            onChange={(event) => {
+              setNumero(event.target.value)
+            }}
+          />
+          <label>Comidas:</label>
+          { comidas.map(comida => {
+            return (
+              <div key={comida.id} className={styles.modalNovoPedido}>
+                <img src={comida.imagem} alt=""/>
+                <p>{comida.nome}</p>
+                <p>{comida.descricao}</p>
+                <p>{comida.preco}</p>
+                <input type="number" className={styles.inputQuantidade}/>
+              </div>
+            )}
+          )}
+
+          <label>Bebidas:</label>
+          { bebidas.map(bebida => {
+            return (
+              <div key={bebida.id} className={styles.modalNovoPedido}>
+                <img src={bebida.imagem} alt=""/>
+                <p>{bebida.tipo}</p>
+                <p>{bebida.nome}</p>
+                <p>{bebida.preco}</p>
+                <input type="number" className={styles.inputQuantidade}/>
+              </div>
+            )}
+          )}
+        </div>
+      </Modal>
       <Footer />
 
     </>
