f881d2e4d9aa532f3f78691c32326972d953357c
Bárbara Cruz <ba.luizacruz@gmail.com>
2022-01-09 18:43:07 -0300

    feat:  assets e estilo footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,19 @@
 import React from "react";
 
 import styles from "./styles.module.css";
+import instagram from "../../assets/instagram.png";
+import facebook from "../../assets/facebook.png";
+import whatsapp from "../../assets/whatsapp.png";
 
 class Footer extends React.Component {
   render() {
     return (
       <footer className={styles.footer}>
-        <p className={styles.text}>
-          Teste Footer
-        </p>
+        <div className={styles.contato}>
+          <img className={styles.img} src={instagram} alt=""></img>
+          <img className={styles.img} src={facebook} alt=""></img>
+          <img className={styles.img} src={whatsapp} alt=""></img>
+        </div>
       </footer>
     );
   }
