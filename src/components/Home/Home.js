9a57e4657619aebc2cb7a2c943073aa16d1b9688
Bárbara Cruz <ba.luizacruz@gmail.com>
2022-01-10 23:49:32 -0300

    feat: estilo pedidos

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,15 +2,25 @@ import React from 'react';
 
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
+import Drink from '../../assets/home/drink.jpg'
+import FoodHamb from '../../assets/home/food1.jpg'
+import FoodChips from '../../assets/home/food2.jpg'
+
+import styles from "./styles.module.css";
 
 function Home() {
 
   return (
-    <div>
+    <body>
         <Header />
         <h1>Home</h1>
+        <div className={styles.photos}>
+          <img className={styles.img} src={Drink} alt=""></img>
+          <img className={styles.img} src={FoodHamb} alt=""></img>
+          <img className={styles.img} src={FoodChips} alt=""></img>
+        </div>
         <Footer />
-    </div>
+    </body>
   );
 }
 
