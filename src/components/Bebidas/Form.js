4afb7cb7356ebcef49791f9770a6c21abfe8c655
Britodebora <debora.cbrito14@gmail.com>
2022-01-11 00:20:48 -0300

    feat: Listar Bebidas

---
+++ src/components/Bebidas/Form.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import styles from './styles.module.css';
+
+function FormBebidas() {
+  return (
+    <div>
+      <form>
+          
+      </form>
+    </div>
+  );
+}
+
+export default FormBebidas;
